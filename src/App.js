c6214e86eff03c5483890ff1683e33ac1f4574ec
oderathegreat <oderathegreat@gmail.com>
2022-08-16 09:44:52 +0300

    username and message variables map

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import {useState} from 'react';
 
 function App() {
 //set chat texts 
-const [chatmessage, setChatMessage] = useState('chatmessage');
+
+
+const [messages, setMessages] = useState([]);
+const [message, setMessage] = useState('');
+
+//set username
 
 const [username, setUsername] = useState('username');
-
 
 
   return (
@@ -25,20 +29,41 @@ const [username, setUsername] = useState('username');
       
       
         </div>
-        <div class="col-10 mb-1 small">Some placeholder content in a paragraph below the heading and date.</div>
+        <div class="col-10 mb-1 small">Enter Username</div>
       </div>
-      <a href="#" class="list-group-item list-group-item-action py-3 lh-sm">
-        <div class="d-flex w-100 align-items-center justify-content-between">
-          <strong class="mb-1">List group item heading</strong>
-          <small class="text-muted">Tues</small>
-        </div>
-        <div class="col-10 mb-1 small">Some placeholder content in a paragraph below the heading and date.</div>
-      </a>
-     
-     
+ 
+    
+      <div class="list-group list-group-flush border-bottom scrollarea">
+   
+   
+      <div className="list-group-item list-group-item-action py-3 lh-sm">
+
+          <strong class="mb-1">Chat Replys</strong>
+
+
+          {message.map(message => {
+                        return (
+                            <div className="list-group-item list-group-item-action py-3 lh-tight">
+                                <div className="d-flex w-100 align-items-center justify-content-between">
+                                    <strong className="mb-1">{message.username}</strong>
+                                </div>
+                                <div className="col-10 mb-1 small">{message.message}</div>
+                            </div>
+                        )
+                    })}
+      
+      </div>
+   
+
+
     </div>
     
+     
     </div>
+
+    </div>
+    
+    
   );
 }
 
